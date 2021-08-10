f3cfac6c81dc07fc11d0cfb0e71bcd25888ad220
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-10 14:37:28 +0100

    Update google-hole.sh

--- google-hole.sh
+++ google-hole.sh
@@ -5,6 +5,9 @@ echo "Installing Docker"
 echo "-----------------"
 sleep 5 &&
 sudo curl -sSL https://get.docker.com | sh && 
+echo "-----------------"
+echo "Docker Installed"
+echo "-----------------"
 sudo curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add - && 
 sudo curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list && 
 sudo apt-get update && 
@@ -22,6 +25,6 @@ echo "-----------------
 echo "Installing PiHole" 
 sleep 5
 sudo docker run -dit --name pihole -p 80:80 -p 53:53 -p 53:53/udp -p 67:67 -p 67:67/udp pihole/pihole &&
-echo "PiHole Has been installed" &&
-echo "Pihole Installed " &&
-echo "Access Pihole dashboard: http://<tailscale_ip:88/admin "
+echo "PiHole Has been installed" 
+echo "Pihole Installed"
+echo "Access Pihole dashboard >>>   http://<tailscale_ip:88/admin   <<<"
