3b821f07b8ce15d357270d477fd5132cd0c285af
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-10 14:29:45 +0100

    Update google-hole.sh

--- google-hole.sh
+++ google-hole.sh
@@ -1,16 +1,24 @@
 #! /usr/bin/bash
 sudo apt-get update && 
+echo "-----------------"
+echo "Installing Docker"
+sleep 5 &&
 sudo curl -sSL https://get.docker.com | sh && 
 sudo curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add - && 
 sudo curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list && 
 sudo apt-get update && 
+echo "-----------------"
+echo "Installing Tail Scale"
+sleep 5 &&
 sudo apt-get install tailscale && 
+echo "------------------"
+echo "Be ready to approve Tail Scale Connection"
+sleep 5 &&
 sudo tailscale up &&
-echo 'Link TailScale account (waiting 20 sec)'&&
-sleep 20 &&
+echo "-----------------
+echo "Installing PiHole" &&
+sleep 
 sudo docker run -dit --name pihole -p 80:80 -p 53:53 -p 53:53/udp -p 67:67 -p 67:67/udp pihole/pihole &&
 echo 'PiHole Has been installed' &&
-sudo docker pull pihole/pihole &&
-sudo docker run -dit -nname pihole -p 80:80 -p 53:53 -p 53:53/udp -p 67:67 -p 67:67/udp pihole/pihole:latest &&
-echo "Pihole Installed "
-sleep 10
+echo "Pihole Installed " &&
+echo "Access Pihole dashboard: http://<tailscale_ip:88/admin "
