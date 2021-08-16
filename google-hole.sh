3f674953443b89c78867365e8a7fc3136683df21
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-16 09:49:52 +0100

    Update google-hole.sh

--- google-hole.sh
+++ google-hole.sh
@@ -1,30 +1,7 @@
-#! /usr/bin/bash
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add - && 
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list && 
 sudo apt-get update && 
-echo "-----------------"
-echo "Installing Docker"
-echo "-----------------"
-sleep 5 &&
-sudo curl -sSL https://get.docker.com | sh && 
-echo "-----------------"
-echo "Docker Installed"
-echo "-----------------"
-sudo curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add - && 
-sudo curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list && 
-sudo apt-get update && 
-echo "---------------------"
-echo "Installing Tail Scale"
-echo "---------------------"
-sleep 5 &&
+curl -sSL https://get.docker.com | sh && 
+sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent &&
 sudo apt-get install tailscale && 
-echo "-----------------------------------------"
-echo "Be ready to approve Tail Scale Connection"
-echo "-----------------------------------------"
-sleep 5 &&
-sudo tailscale up &&
-echo "-----------------
-echo "Installing PiHole" 
-sleep 5
-sudo docker run -dit --name pihole -p 80:80 -p 53:53 -p 53:53/udp -p 67:67 -p 67:67/udp pihole/pihole:latest
-echo "PiHole Has been installed" 
-echo "Pihole Installed"
-echo "Access Pihole dashboard https://[tailscale_assigned_ip]:8/admin"
+sudo tailscale up
