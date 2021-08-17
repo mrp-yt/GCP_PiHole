21dc0049bf8d0e3540f86f9249898cd12241c3f3
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-18 00:04:49 +0100

    Update google-hole.sh

--- google-hole.sh
+++ google-hole.sh
@@ -3,6 +3,6 @@ curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list && 
 sudo apt-get update && 
 curl -sSL https://get.docker.com | sh && 
-sudo docker run -dit --name pihole -p 80:80 -p 53:53 -p 53:53/udp -p 67:67 -p 67:67/udp -e WEBPASSWORD=password pihole/pihole &&
+sudo docker run -dit --name pihole -p 80:80 -p 53:53 -p 53:53/udp -p 67:67 -p 67:67/udp -e WEBPASSWORD= pihole/pihole &&
 sudo apt-get install tailscale && 
 sudo tailscale up
